e66dc31f9f1611b9ecb585ab299e0e2983159cdc
Svyatoslav Andreev <andreev.sv@phystech.edu>
2026-09-09 13:25:32 +0300

    Added MyMath class & Gcd method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
+import java.util.Scanner;
+
+class MyMath{
+    public static int Gcd(int a, int b){
+        if (a>b){
+            a+=b;
+            b = a-b;
+            a-=b;
+        }
+        if (a==0) return b;
+        return Gcd(b%a, a);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        CalculateGcd();
+    }
+
+    private static void CalculateGcd(){
+        MyMath mm = new MyMath();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt(), b = scanner.nextInt();
+        System.out.println(mm.Gcd(a, b));
     }
 }
